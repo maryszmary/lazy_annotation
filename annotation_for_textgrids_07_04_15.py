40bed55e125ad5d17d34bfa48d28d0d7488c6a03
maryszmary <masha.shejanova@gmail.com>
2017-01-28 20:37:48 +0300

    started refactoring

--- annotation_for_textgrids_07_04_15.py
+++ annotation_for_textgrids_07_04_15.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-# TODO: refactor
+# refactoring
 import codecs
 import re
 import sys
 
 
+MODIFICARORS = u'\'ˤʃʷˀ:͡'
+STOPS = u'p\'k\'q\'dt\'b'
 the_name = raw_input('Please type the filename ').decode(sys.stdin.encoding)
 sourcetier = input('Type the number of the source tier ')
 
@@ -16,6 +18,7 @@ def file_array(name):
             text.append(line.strip())
     return text
 
+
 def write_from_array(name, array):
     f1 = codecs.open(name,'w','utf-16-be')
     for el in array:
@@ -23,66 +26,74 @@ def write_from_array(name, array):
     f1.close()
     return f1
 
+
+def get_tiernames(lines):
+    '''названия слоёв'''
+    tier_words = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
+                                lines[3], flags = re.U) + u':'
+    tier_sounds = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
+                                lines[4], flags = re.U) + u':'
+    tier_stops = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
+                                lines[5], flags = re.U) + u':'
+    tier_diffwords = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
+                                lines[sourcetier + 2], flags = re.U) + u':'
+    return tier_words, tier_sounds, tier_stops, tier_diffwords
+
+
+def get_intr(lines):
+    intr = []
+    for el in lines:
+        if el != tier_diffwords: # commented out: 27.01.17 (UNCOMMENTED BACK TEMPORARY)
+        # if el not in [tier_diffwords, tier_words, tier_stops, tier_sounds]: commented out for the purpose of KOSTYLI!!!1
+            intr.append(el)
+        else:
+            break
+    return intr
+
+
+def get_all_sets(lines):
+    '''все сеты лексем'''
+    n = 0
+    all_sets = []
+    while n < len(lines):
+        if tier_diffwords in lines[n]:
+            diff_words = []
+            m = n
+            while m < len(lines):
+                if m == n or tier_diffwords not in lines[m]:
+                    diff_words.append(lines[m])
+                    m += 1
+                else:
+                    break
+            all_sets.append(diff_words)
+        n += 1
+    return all_sets
+
+tier_words, tier_sounds, tier_stops, tier_diffwords = get_tiernames(lines)
 lines = file_array(the_name)
-
-#названия слоёв
-
-tier_words = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
-                            lines[3], flags = re.U) + u':'
-tier_sounds = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
-                            lines[4], flags = re.U) + u':'
-tier_stops = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
-                            lines[5], flags = re.U) + u':'
-tier_diffwords = u'! ' + re.sub('\"IntervalTier\" \"(.+)\".+', '\\1',
-                            lines[sourcetier + 2], flags = re.U) + u':'
 
 # TEMPORARY FIX
 lines = lines[:9] + ['', '', tier_diffwords, '',  '\"\"',
                       '', tier_diffwords, '', '\"\"', ''] + lines[9:]
+# TEMPORARY FIX
 
-
-with codecs.open('/tmp/try', 'w') as f:
-    f.write('\n'.join(lines))
-
-intr = []
-for el in lines:
-    if el != tier_diffwords: # commented out: 27.01.17
-    # if el not in [tier_diffwords, tier_words, tier_stops, tier_sounds]: commented out for the purpose of KOSTYLI!!!1
-        intr.append(el)
-    else:
-        print('ended intr. length of intr: ' + str(len(intr)))
-        print('\n'.join(intr))
-        break
-
-#все сеты лексем
-
-n = 0
-all_sets = []
-while n < len(lines):
-    if tier_diffwords in lines[n]:
-        diff_words = []
-        m = n
-        while m < len(lines):
-            if m == n or tier_diffwords not in lines[m]:
-                diff_words.append(lines[m])
-                m += 1
-            else:
-                break
-        all_sets.append(diff_words)
-    n += 1
+intr = get_intr(lines)
+all_sets = get_all_sets(lines)
 
 #аннотация слов
 
-for arr in all_sets:
-    if arr[2] != '\"\"':
-        i = 0
-        ii = 1
-        while i < len(arr):
-            if arr[i] == tier_words:
-                ii += 1
-                if ii%2 == 0:
-                    arr[i + 2] = arr[2]
-            i += 1
+def word_annotation(all_sets, tier_words):
+    for arr in all_sets:
+        if arr[2] != '\"\"':
+            i = 0
+            ii = 1
+            while i < len(arr):
+                if arr[i] == tier_words:
+                    ii += 1
+                    if ii%2 == 0:
+                        arr[i + 2] = arr[2]
+                i += 1
+    return all_sets
 
 #new_lines - это весь файл
 
@@ -119,7 +130,6 @@ while n < len(new_lines):
 
 #здесь аннотируется второй слой
 
-modificators = u'\'ˤʃʷˀ:͡'
 
 for arr in all_words:
     if arr[2] != '\"\"' and tier_sounds in arr:
@@ -127,47 +137,33 @@ for arr in all_words:
         sounds = []
         sound = 0
         for l in range(len(word)):
-##            if sound != 0 and word[l] == u's' and word[l - 1] == u't':
-##                sounds[sound - 1] += word[l]
-##            elif sound != 0 and word[l] == u'ɬ' and word[l - 1] == u't':
-##                sounds[sound - 1] += word[l]
-##            elif word[l] not in modificators:
-##                sounds.append(word[l])
-##                sound += 1            
-##            else:
-##                if sound != 0:
-##                    sounds[sound - 1] += word[l]
             if (sound != 0 and ((word[l] == u's' and word[l - 1] == u't') or
                                 (word[l] == u'ɬ' and word[l - 1] == u't') or
                                 (word[l] == u'χ' and word[l - 1] == u'͡')) or
-                word[l] in modificators):
+                word[l] in MODIFICARORS):
                 sounds[sound - 1] += word[l]
             else:
                 sounds.append(word[l])
                 sound += 1            
         #в этой части кода получившиеся звуки приписываются интервалам в файле
-        #здесь всё нормально
-        #не надо ничего трогать
         boundary = 0
         for w in range(len(arr)):
             if tier_sounds == arr[w]:
                 if boundary < len(sounds):
                     arr[w + 2] = u'\"' + sounds[boundary] + u'\"'
                     boundary += 1
-                    
+                
+
 #здесь аннотируется третий слой
-
-stops = u'p\'k\'q\'dt\'b'
-
 for arr in all_words:
     for w in range(len(arr)):
         if arr[w] == tier_sounds:
             thesound = arr[w + 2].strip('"')
-            if thesound != u'' and thesound in stops:
+            if thesound != u'' and thesound in STOPS:
                 if tier_stops not in arr[w + 7:]:
-                    print "SOMETHING GONE WRONG at", w
-                    print u'\n'.join(arr[w:])
-                    print "UTTERLY"
+                    print("SOMETHING GONE WRONG at " + str(w))
+                    print(u'\n'.join(arr[w:]))
+                    print("UTTERLY")
                 if tier_stops in arr[w + 7:]:
                     if arr[w + 4] == tier_stops and arr[w + 8] == tier_stops:
                         arr[w + 6] = u'\"C_' + thesound + u'\"'
@@ -179,12 +175,11 @@ for arr in all_words:
 
 #здесь создаётся окончательная версия массива для записи в новый файл
 
-
-# here i change something
-
 for el in all_words:
     intr += el 
+
 # TEMPORARY FIX
 intr = intr[:9] + intr[18:]       
+# TEMPORARY FIX
 
 write_from_array(u'annotated_' + the_name + u'.TextGrid', intr)
